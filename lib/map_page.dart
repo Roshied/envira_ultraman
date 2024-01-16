e5a96411b6836438c2e78aea6c99c4a2ad4c6174
Roshied Mohammad <roshiedmohammad@ymail.com>
2024-01-16 20:10:01 +0700

    navbar

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:collection';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter_google_places_hoc081098/flutter_google_places_hoc081098.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart' as point;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_flutter_platform_interface/src/types/polyline.dart'
-    as poly;
+as poly;
 import 'package:google_api_headers/google_api_headers.dart';
 import 'package:google_maps_webservice/directions.dart';
 import 'package:google_maps_webservice/places.dart';
@@ -22,14 +24,14 @@ class _SearchMapState extends State<SearchMap> {
   loc.Location _locationController = new loc.Location();
 
   final Completer<GoogleMapController> _mapController =
-      Completer<GoogleMapController>();
+  Completer<GoogleMapController>();
 
   GoogleMapController? mapController;
   String location = "Search Location";
   static const LatLng _pGooglePlex =
-      LatLng(-7.268588481297382, 112.78431469818055);
+  LatLng(-7.268588481297382, 112.78431469818055);
   static const LatLng _pApplePark =
-      LatLng(-7.264635809343702, 112.75851922331852);
+  LatLng(-7.264635809343702, 112.75851922331852);
   LatLng? _currentP = null;
 
   Map<PolylineId, poly.Polyline> polylines = {};
@@ -38,101 +40,118 @@ class _SearchMapState extends State<SearchMap> {
   void initState() {
     super.initState();
     getLocationUpdates().then((_) => {
-          getPolyLinePoints().then((coordinates) => {
-                generatePolyLineFromPoints(coordinates),
-              }),
-        });
+      getPolyLinePoints().then((coordinates) => {
+        generatePolyLineFromPoints(coordinates),
+      }),
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("ENVIRA"),
-          backgroundColor: Colors.yellowAccent,
-        ),
-        body: _currentP == null
-            ? const Center(
-                child: Text("Loading..."),
-              )
-            : Stack(
-                children: [
-                  GoogleMap(
-                    onMapCreated: ((GoogleMapController controller) =>
-                        _mapController.complete(controller)),
-                    initialCameraPosition: CameraPosition(
-                      target: _pGooglePlex,
-                      zoom: 15,
-                    ),
-                    markers: {
-                      Marker(
-                          markerId: MarkerId("_currentLocation"),
-                          icon: BitmapDescriptor.defaultMarker,
-                          position: _currentP!),
-                      Marker(
-                          markerId: MarkerId("_sourceLocation"),
-                          icon: BitmapDescriptor.defaultMarker,
-                          position: _pGooglePlex),
-                    },
-                    polylines: Set<poly.Polyline>.of(polylines.values),
-                  ),
-                  Positioned(
-                      top: 10,
-                      child: InkWell(
-                        onTap: () async {
-                          var place = await PlacesAutocomplete.show(
-                              context: context,
-                              apiKey: GOOGLE_MAPS_API_KEY,
-                              mode: Mode.overlay,
-                              types: [],
-                              strictbounds: false,
-                              components: [Component(Component.country, 'id')],
-                              onError: (err) {
-                                print(err);
-                              });
+      appBar: AppBar(
+        title: Text("ENVIRA"),
+        backgroundColor: Colors.yellowAccent,
+      ),
+      body: _currentP == null
+          ? const Center(
+        child: Text("Loading..."),
+      )
+          : Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: ((GoogleMapController controller) =>
+                _mapController.complete(controller)),
+            initialCameraPosition: CameraPosition(
+              target: _pGooglePlex,
+              zoom: 15,
+            ),
+            markers: {
+              Marker(
+                  markerId: MarkerId("_currentLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _currentP!),
+              Marker(
+                  markerId: MarkerId("_sourceLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _pGooglePlex),
+            },
+            polylines: Set<poly.Polyline>.of(polylines.values),
+          ),
+          Positioned(
+              top: 10,
+              child: InkWell(
+                onTap: () async {
+                  var place = await PlacesAutocomplete.show(
+                      context: context,
+                      apiKey: GOOGLE_MAPS_API_KEY,
+                      mode: Mode.overlay,
+                      types: [],
+                      strictbounds: false,
+                      components: [Component(Component.country, 'id')],
+                      onError: (err) {
+                        print(err);
+                      });
 
-                          if (place != null) {
-                            setState(() {
-                              location = place.description.toString();
-                            });
+                  if (place != null) {
+                    setState(() {
+                      location = place.description.toString();
+                    });
 
-                            final plist = GoogleMapsPlaces(
-                              apiKey: GOOGLE_MAPS_API_KEY,
-                              apiHeaders: await GoogleApiHeaders().getHeaders(),
-                            );
-                            String placeid = place.placeId ?? "0";
-                            final detail =
-                                await plist.getDetailsByPlaceId(placeid);
-                            final geometry = detail.result.geometry!;
-                            final lat = geometry.location.lat;
-                            final lng = geometry.location.lng;
-                            var newlatlng = LatLng(lat, lng);
+                    final plist = GoogleMapsPlaces(
+                      apiKey: GOOGLE_MAPS_API_KEY,
+                      apiHeaders: await GoogleApiHeaders().getHeaders(),
+                    );
+                    String placeid = place.placeId ?? "0";
+                    final detail =
+                    await plist.getDetailsByPlaceId(placeid);
+                    final geometry = detail.result.geometry!;
+                    final lat = geometry.location.lat;
+                    final lng = geometry.location.lng;
+                    var newlatlng = LatLng(lat, lng);
 
-                            mapController?.animateCamera(
-                                CameraUpdate.newCameraPosition(CameraPosition(
-                                    target: newlatlng, zoom: 13)));
-                          }
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.all(15),
-                          child: Card(
-                            child: Container(
-                              padding: EdgeInsets.all(0),
-                              width: MediaQuery.of(context).size.width - 40,
-                              child: ListTile(
-                                title: Text(
-                                  location,
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                                trailing: Icon(Icons.search),
-                                dense: true,
-                              ),
-                            ),
-                          ),
+                    mapController?.animateCamera(
+                        CameraUpdate.newCameraPosition(
+                            CameraPosition(target: newlatlng, zoom: 13)));
+                  }
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Card(
+                    child: Container(
+                      padding: EdgeInsets.all(0),
+                      width: MediaQuery.of(context).size.width - 40,
+                      child: ListTile(
+                        title: Text(
+                          location,
+                          style: TextStyle(fontSize: 18),
                         ),
-                      )),
-                ],
-              ));
+                        trailing: Icon(Icons.search),
+                        dense: true,
+                      ),
+                    ),
+                  ),
+                ),
+              )),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_hospital_rounded),
+            label: 'Kesehatan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Pengaturan',
+          ),
+        ],
+      ),
+    );
   }
 
   Future<void> _cameraToPosition(LatLng pos) async {
@@ -181,7 +200,7 @@ class _SearchMapState extends State<SearchMap> {
     List<LatLng> polylineCoordinates = [];
     point.PolylinePoints polylinePoints = point.PolylinePoints();
     point.PolylineResult result =
-        await polylinePoints.getRouteBetweenCoordinates(
+    await polylinePoints.getRouteBetweenCoordinates(
       GOOGLE_MAPS_API_KEY,
       point.PointLatLng(_currentP!.latitude, _currentP!.longitude),
       point.PointLatLng(_pGooglePlex.latitude, _pGooglePlex.longitude),
